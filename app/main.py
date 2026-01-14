493f24869158a278206cee2e1ebedb867f6145a3
Iltan <nl.iltanix@gmail.com>
2026-01-14 19:52:34 +0200

    add main

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from .database import models, database
+from .endpoints import training, inference, monitoring
+
+# Створення таблиць (якщо не існують)
+models.Base.metadata.create_all(bind=database.engine)
+
+app = FastAPI(title="CallTech ML API")
+
+app.include_router(training.router, tags=["Training"])
+app.include_router(inference.router, tags=["Inference"])
+app.include_router(monitoring.router, tags=["Monitoring"])
+
+@app.get("/")
+def root():
+    return {"message": "CallTech ML API is running"}
