e9f3b85c5adabbd6d2b632922e999b8a0910e2dc
Iltan <nl.iltanix@gmail.com>
2026-01-21 12:07:08 +0200

    Update utils

--- app/services/utils.py
+++ app/services/utils.py
@@ -16,16 +16,17 @@ def prepare_data_for_training(df: pd.DataFrame):
 
 def generate_evidently_report(reference_data: pd.DataFrame, current_data: pd.DataFrame, target_col='response'):
     common_cols = [c for c in reference_data.columns if c in current_data.columns and c != 'id']
-    
     ref = reference_data[common_cols].copy()
     cur = current_data[common_cols].copy()
     
-    # Report generation
-    report = Report(metrics=[
-        DataDriftPreset(), 
-        DataQualityPreset()
-    ])
+    # Data Drift
+    drift_report = Report(metrics=[DataDriftPreset()])
+    drift_report.run(reference_data=ref, current_data=cur)
+    drift_report.save_html("app/reports/data_drift_report.html")
     
-    report.run(reference_data=ref, current_data=cur)
-    report.save_html("data_drift_report.html")
-    return "Report generated: data_drift_report.html"
+    # Data Quality
+    quality_report = Report(metrics=[DataQualityPreset()])
+    quality_report.run(reference_data=ref, current_data=cur)
+    quality_report.save_html("app/reports/data_quality_report.html")
+    
+    return "Reports generated: data_drift_report.html, data_quality_report.html"
